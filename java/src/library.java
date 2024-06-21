3452e52b99bebde6325863e5b36bc9acb626070d
BhuvanR75 <bhuvan.ravi075@gmail.com>
2024-06-21 23:09:42 +0530

    lib

--- java/src/library.java
+++ java/src/library.java
@@ -1,30 +1,47 @@
 import java.util.Scanner;
 
-public class library {
-
-    public class library1{
-        public String addbook(int n)
-        {    int c=n;
-            String books[];
-            books[0]="ram";
-           
-            for(int i=0;i<2;i++)
-            {
-                books[i];
-
-            }
-            
-            return "S";
+import javax.annotation.processing.SupportedOptions;
+class library1{
+    
+    String books[]=new String[1000];
+    public void addbook(int n)
+    {   
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter the names of books");
+        for(int i =0;i<n;i++)
+        {
+            String m = sc.nextLine();
+            books[i] = m;
+        }
+        System.out.println("the total books are");
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(books[i]);
+        }
+        
+    }
+    public void rem(int n)
+    {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        System.out.println("the remaing books are");
+        for(int i=0;i<n+1;i++)
+        {
+           if(i==b-1)
+           continue;
+           System.out.println(books[i]);
         }
     }
+}
+public class library {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int b = sc.nextInt();
-        int r[];
-        for()
-
-        
-        
+         
+        library1 lib =new library1();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("the no of books:");
+        int n = sc.nextInt();
+        lib.addbook(n); 
+        lib.rem(n);
         
     }
     
